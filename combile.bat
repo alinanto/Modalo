e1e3ea7856e137f939a9ef0ce7fa92aaeead92f4
alinanto <alinanto5@gmail.com>
2024-04-03 17:58:42 +0530

    combile.bat updated with latest include path

--- combile.bat
+++ combile.bat
@@ -2,16 +2,16 @@ cls
 @echo OFF
 
 :: combiling modbus library - uncomment only for recombiling dll library
-:: gcc -I includes/ -o bin/libmodbus.dll -s -shared src/modbus.c src/modbus-rtu.c src/modbus-tcp.c src/modbus-data.c -lws2_32 -D DLLBUILD
+gcc -I src/includes/ -o bin/libmodbus.dll -s -shared src/modbus.c src/modbus-rtu.c src/modbus-tcp.c src/modbus-data.c -lws2_32 -D DLLBUILD
 
 :: combiling cJSON - JSON Parsing library -  - always recombileuncomment only for recombiling dll library
-:: gcc -I includes/ -o bin/libcJSON.dll -s -shared src/cJSON.c
+gcc -I src/includes/ -o bin/libcJSON.dll -s -shared src/cJSON.c
 
 :: combiling library for error handling - always recombile
-:: gcc -I includes/ -o bin/liberror-modalo.dll -s -shared src/modaloError.c -D MODALO_EXPORT
+ gcc -I src/includes/ -o bin/liberror-modalo.dll -s -shared src/modaloError.c -D MODALO_EXPORT
 
 :: combiling library for parsing - always recombile
-gcc -L bin/ -I includes/ -o bin/libparse-modalo.dll -s -shared src/modaloParse.c -lcJSON -lerror-modalo -D CJSON_IMPORT_SYMBOLS -D MODALO_EXPORT
+gcc -L bin/ -I src/includes/ -o bin/libparse-modalo.dll -s -shared src/modaloParse.c -lcJSON -lerror-modalo -D CJSON_IMPORT_SYMBOLS -D MODALO_EXPORT
 
 :: combiling main application modalo - always recombile
-gcc -L bin/ -I includes/ -o bin/Modalo.exe -w -s src/main.c -lmodbus -lparse-modalo -lerror-modalo
+gcc -L bin/ -I src/includes/ -o bin/Modalo.exe -w -s src/main.c -lmodbus -lparse-modalo -lerror-modalo
